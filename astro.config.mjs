63cea8bff5a7718335e7aa6d02d4ac017b4afa81
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-21 18:42:19 +0330

    feat: add i18n config and update dependencies for Tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'fa', 'tr'],
+    routing: {
+      prefixDefaultLocale: true,
+    }
+  }
+});
